e080d51f3f85f14cebdd5717bd5d129f3b7c86f8
Lucas Alves <alvez.lucas2702@gmail.com>
2024-11-22 14:07:12 -0300

    adicionando servicos do google

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -26,11 +26,32 @@ export async function uploadImagem(req, res) {
 
     try {
         const postCriado = await criarPost(novoPost);
-        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
-        fs.renameSync(req.file.path, imagemAtualizada);
-        res.status(200).json(postCriado);
-    } catch (erro) {
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
+        fs.renameSync(req.file.path, imagemAtualizada)
+        res.status(200).json(postCriado);  
+    } catch(erro) {
         console.error(erro.message);
-        res.status(500).json({"Erro": "Falha na requisição"})
-    } 
+        res.status(500).json({"Erro":"Falha na requisição"})
+    }
+}
+
+export async function atualizarNovoPost(req, res) {
+    const id = req.params.id;
+    const urlImagem = `http://localhost:3000/${id}.png`
+    try {
+        const imgBuffer = fs.readFileSync(`uploads/${id}.png`)
+        const descricao = await gerarDescricaoComGemini(imgBuffer)
+
+        const post = {
+            imgUrl: urlImagem,
+            descricao: descricao,
+            alt: req.body.alt
+        }
+
+        const postCriado = await atualizarPost(id, post);
+        res.status(200).json(postCriado);  
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na requisição"});
+    }
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,24 +1,33 @@
 import express from "express";
-import multer from "multer";
-import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+import multer from "multer"; 
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postsController.js";
+import cors from "cors";
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename: function (req, file, db) {
-        cb(null, file.originalname);
-    }
-});
-
-const upload = multer({dest: "./uploads", storage})
-
-const routes = (app) => {
-    app.use(express.json()); //Conversao para json
-    app.get("/posts", listarPosts);
-    app.post("/posts", postarNovoPost);
-    app.post("/upload", upload.sigle("imagem"), uploadImagem);
+const corsOptions = {
+  origin: "http://localhost:8000",
+  optionsSuccessStatus: 200
 }
 
-export default routes;
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); 
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
 
+const upload = multer({ storage: storage });
+
+const routes = (app) => {
+
+  app.use(express.json());
+  app.use(cors(corsOptions))
+
+  app.get("/posts", listarPosts);
+  app.post("/posts", postarNovoPost); 
+  app.post("/upload", upload.single("imagem"), uploadImagem); 
+  app.put("/upload/:id", atualizarNovoPost)
+};
+
+export default routes;
